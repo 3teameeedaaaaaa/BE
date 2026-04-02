faa499bf8b92be6defdc95bce3ebedebaa8b35b4
BaeYoonSang <yoonsang9802@naver.com>
2026-04-02 17:37:01 +0900

    3개의 Enum 파일 생성

---
+++ src/main/java/com/example/water/global/common/SessionMode.java
@@ -0,0 +1,14 @@
+package com.example.water.global.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SessionMode {
+    PRE("",""),
+    POST("","");
+
+    private final String code;
+    private final String text;
+}
