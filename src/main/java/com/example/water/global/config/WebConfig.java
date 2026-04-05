b4e6ac1636b540a8f01ca7577e25c2106694ec6c
bys0212 <yoonsang9802@naver.com>
2026-04-05 11:26:14 +0900

    refactor: CORS 수정

--- src/main/java/com/example/water/global/config/WebConfig.java
+++ src/main/java/com/example/water/global/config/WebConfig.java
@@ -10,11 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://.com"
+                        "http://localhost:5173"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
